f2d1f3dfd40ca57cacd3b362ec88225ae3f3650a
ht1006 <36884859+ht1006@users.noreply.github.com>
2020-05-07 13:51:01 +0100

    test for deployment trial

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World on OpenShift")
+                                    .addInitParam("message", "Hello World on OpenShift, for deployment testing")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
